f2243f381b11d899361fd969dceb89b478a6a737
leow123b <leonardo.cadenasantos@gmail.com>
2024-04-08 10:45:55 -0300

    aula7

---
+++ exer.php
@@ -0,0 +1,65 @@
+<?php
+
+echo "<h1>aula 07<?h1>";
+
+$str = "janela";
+echo $str[-1];
+
+$str = [2] = "0";
+echo $str;
+
+quebra();
+
+echo $str[0] ."oao";
+
+quebra();
+$tamanho = strlen(Str);
+echo "tamanho STR: ". $tamanho; 
+
+quebra();
+$num = 55;
+echo <br>o resultado e : $num.";
+echo "<br>o resultado e : {$num}.";
+echo <br>o resultado e. $num.";
+
+quebra();
+//heredoc
+$novaVar = 80;
+
+$heredoc = <<<TESTE
+         
+       <h2>o titulo da pagina </h2>
+       <p> o texto da pagina</p>
+
+TESTE;
+
+
+   function  titulo($texto){
+    Stamanho = 10 + strilen ($tesxto);
+    echo "\n<br>";
+    echo str_pad("", $tamanho)
+   }
+/* 
+   number format() // formatar numero
+   printf() // imprimir e formatar 
+   printf_r() // mostrar array
+
+   vae_dump() // mostra array e tipo de variaveis 
+   trim)()
+   litrim()
+   rtrin()
+
+   str_word_count() // contagem de palavras 
+   explode() // quebra elas em array ["eu" , "estou"
+   str_split() //
+
+
+    
+
+// nowdoc 
+
+
+
+
+
+?>
